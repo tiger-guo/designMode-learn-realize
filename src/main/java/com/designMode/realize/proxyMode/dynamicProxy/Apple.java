3217105bc98943a6386134fbc6114592a46f453b
liuguohu <1774687347@qq.com>
2020-03-17 18:49:00 +0800

    设计模式-代理模式-动态代理-JDK自带方法 编码完成

---
+++ src/main/java/com/designMode/realize/proxyMode/dynamicProxy/Apple.java
@@ -0,0 +1,16 @@
+package com.designMode.realize.proxyMode.dynamicProxy;
+
+/**
+ * @program: designMode-learn&realize
+ * @description:
+ * @author: liuguohu
+ * @create: 2020-03-17 18:27
+ **/
+
+public class Apple implements Fruit {
+
+    @Override
+    public void show() {
+        System.out.println("-------- invoked --------");
+    }
+}
---
+++ src/test/java/com/designMode/realize/proxyMode/dynamicProxy/ReflectTest.java
@@ -0,0 +1,22 @@
+package com.designMode.realize.proxyMode.dynamicProxy;
+
+import org.junit.Test;
+
+import java.lang.reflect.InvocationTargetException;
+
+/**
+ * @program: designMode-learn&realize
+ * @description:
+ * @author: liuguohu
+ * @create: 2020-03-17 18:35
+ **/
+
+public class ReflectTest {
+
+    @Test
+    public void test() throws InvocationTargetException, IllegalAccessException {
+        //注意一定要返回接口，不能返回实现类否则会报错
+        Fruit fruit = (Fruit) DynamicAgent.agent(Fruit.class, new Apple());
+        fruit.show();
+    }
+}
